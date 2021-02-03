8492670e5f8134a344d1cdb2b83f13824665b72a
HimanthSura <78466704+HimanthSura@users.noreply.github.com>
2021-02-03 15:17:52 +0530

    Add files via upload

---
+++ madlib1.py
@@ -0,0 +1,21 @@
+fillup1 = input("blank 1 :")
+fillup2 = input("blank 2 :")
+fillup3 = input("blank 3 :")
+fillup4 = input("blank 4 :")
+fillup5 = input("blank 5 :")
+fillup6 = input("blank 6 :")
+
+
+
+
+
+
+
+madlib = f"Opera {fillup1} to a dramatic art form, originating in Europe, in which the emotional content is\
+{fillup2} to the audience as much through music, both vocal and instrumental, as it is through\
+the lyrics. by {fillup3}, in musical theater an actor's dramatic {fillup4} is primary, and the\
+music plays a lesser role The drama in opera is presented using the primary elements of theater \
+such as scenery, costumes, and acting. {fillup5}, the words of the opera, or libretto, are sung\
+rather than spoken. The singers are accompanied by {fillup6} musical ensemble ranging from a small\
+instrumental ensemble to a full symphonic orchestra."
+print(madlib)
